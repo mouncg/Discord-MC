417629dfba4a7ab95d164ad246a531c2847aee53
TotherMeteor <33614909+TotherMeteor@users.noreply.github.com>
2020-01-31 11:48:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ bot.on('chat', (username, message) => {
             if (err)
                 console.log(err);
             else
+                return
                 //players = { username: { uuid: res.id, joins: 1, leaves: 0, kills: 0, deaths: 0 } }
                 //fs.writeFile('./players.json', JSON.stringify(client.players, null, 4));
         });
