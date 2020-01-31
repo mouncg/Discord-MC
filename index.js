e9270e4d98b872a3ecf00500ba6b1a680b853b65
TotherMeteor <byarssean924@gmail.com>
2020-01-30 20:28:11 -0600

    Added code to create the discord & mineflayer bot

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-/*
-    Basically, I've got everything layed
-    out, I'll be implementing actual code
-    into this file when I get home.
+//Import files & packages
+const discord = require('discord.js');
+const mineflayer = require('mineflayer');
+const client = new Discord.Client({ disableEveryone: true })
+const fs = require('fs');
+const config = require('./config.json');
+const players = require('./players.json');
 
-*/
+//Global Variables
+var token = config['token'];
+var ip = config['ip'];
+var port = config['port'];
+var user = config['email'];
+var pw = config['password'];
+
+
+//Creates the minecraft bot & logs into the server
+const bot = mineflayer.createBot({
+
+    host: ip,
+    port: port,
+    username: user,
+    password: pw,
+    version :'1.12.2'//You can change this to anything below 1.12.2, that's currently the highest version mineflayer supports
+
+})
+
+//Sends a message once the bot loads correctly
+client.on('ready', () => {
+
+    console.log('Successfully loaded...')
+
+})
+
+client.login(token)
+
